2635f3c9e3dc39c6efda3463a87cc6729257a653
lordofdestiny <42172334+lordofdestiny@users.noreply.github.com>
2021-11-21 11:03:13 +0100

    Refactored functions for performance

--- OO1Domaci2V3/Rec.cpp
+++ OO1Domaci2V3/Rec.cpp
@@ -3,35 +3,28 @@
 
 int Rec::countSyllables() const {
 	int count = 0;
-	int len = static_cast<int>(word.length());
-	for (int i = 0; i < len; i++) {
-		bool prevNotVocal = i > 0 ?
-			!isVocal(word[i - 1]) : true;
-		bool nextNotVocal = i < len - 1 ?
-			!isVocal(word[i + 1]) : true;
-		if (isVocal(word[i]) ||
-			(isSonant(word[i]) && prevNotVocal && nextNotVocal)) {
+	for (int i = 0; i < +*this; ++i) {
+		bool prevNotVocal = i == 0 || !vocals(word[i - 1]);
+		bool nextNotVocal = i == +*this - 1 || !vocals(word[i + 1]);
+
+		if (vocals(word[i]) || sonants(word[i]) && prevNotVocal && nextNotVocal) {
 			++count;
 		}
 	}
 	return count;
 }
 
-
 int Rec::getNthSyllableCarrier(int n) const {
 	int count = 0, result = -1;
-	int len = static_cast<int>(word.length());
-	int resultCount = n >= 0 ? n : ~*this + n;
-	for (int i = 0; i < len; i++) {
-		bool prevNotVocal = i > 0 ?
-			!vocals.has(word[i - 1]) : true;
-		bool nextNotVocal = i < len - 1 ?
-			!vocals.has(word[i + 1]) : true;
-		if (isVocal(word[i]) ||
-			(isSonant(word[i]) && prevNotVocal && nextNotVocal)) {
+	int resultCount = n + 1 + ((n < 0) * ~*this);
+	for (int i = 0; i < +*this; ++i) {
+		bool prevNotVocal = i == 0 || !vocals(word[i - 1]);
+		bool nextNotVocal = i == +*this - 1 || !vocals(word[i + 1]);
+
+		if (vocals(word[i]) || sonants(word[i]) && prevNotVocal && nextNotVocal) {
 			++count;
 		}
-		if (count - 1 == resultCount) {
+		if (count == resultCount) {
 			result = i;
 			break;
 		}
@@ -43,15 +36,14 @@ int Rec::getNthSyllableCarrier(int n) const {
 bool Rec::rhymes(Rec const& first, Rec const& second) {
 	int x = (~first == 1 || ~second == 1) ? -1 : -2;
 	int i1 = first(x), i2 = second(x);
-	while (i1 < +first && i2 < +second && first[i1] == second[i2]) {
-		i1++;
-		i2++;
+	while (i1 < +first && i2 < +second &&
+		tolower(first[i1]) == tolower(second[i2])) {
+		i1++; i2++;
 	}
 	return i1 == +first && i2 == +second;
 }
 
 std::string Rec::createWord(std::string const& str) {
-	/*Replace with string stream*/
 	std::string word;
 	for (auto& c : str) {
 		if (std::isalpha(c)) {
