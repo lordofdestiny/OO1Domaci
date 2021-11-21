c661e4c67700596e61f82ab6d73dc11715beecd7
lordofdestiny <42172334+lordofdestiny@users.noreply.github.com>
2021-11-21 10:05:39 +0100

    Constructor  definitions moved to the header file

--- OO1Domaci2V1/Cvet.cpp
+++ OO1Domaci2V1/Cvet.cpp
@@ -1,16 +1,5 @@
 #include "Cvet.h"
 
-Cvet::Cvet(std::string name, unsigned buyPrice, unsigned sellPrice) :
-	name(name),
-	buyPrice(buyPrice),
-	sellPrice(sellPrice) {}
-
-Cvet::Cvet(Cvet&& other) noexcept :
-	name(std::move(other.name)),
-	buyPrice(std::exchange(other.buyPrice, 0)),
-	sellPrice(std::exchange(other.sellPrice, 0))
-{}
-
 Cvet& Cvet::operator=(Cvet const& rhs) {
 	if (this != &rhs) {
 		name = rhs.name;
@@ -19,6 +8,7 @@ Cvet& Cvet::operator=(Cvet const& rhs) {
 	}
 	return *this;
 }
+
 Cvet& Cvet::operator=(Cvet&& rhs) noexcept {
 	if (this != &rhs) {
 		name = std::move(name);
