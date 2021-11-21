d1da52cba3aaf3af2eb55534dd38dac4e5f56dfc
lordofdestiny <42172334+lordofdestiny@users.noreply.github.com>
2021-11-21 10:16:15 +0100

    Removed colon after include on line 3

--- OO1Domaci2V1/OO1Domaci2V1.cpp
+++ OO1Domaci2V1/OO1Domaci2V1.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "Cvet.h"
-#include "Buket.h";
+#include "Buket.h"
 #include "Cvecara.h"
 
 int main()
@@ -12,7 +12,7 @@ int main()
 	Cvet test("Ruza", 230, 500);
 
 	std::cout << (ruza == test ? "Ruza je ista kao test cvet\n" : "Ova dva cveta nisu ista!\n");
-	std::cout << std::string(30, '-') << '\n';
+	std::cout << std::string(30, '-') << '\n';	
 
 	Buket bouquet1;
 	bouquet1.addFlower(gerber);
@@ -49,7 +49,7 @@ int main()
 	cvecara.addBouquet(bouquet3);
 	if (!cvecara.addBouquet(bouquet4)) {
 		std::cout << "Buket nije dodat jer ne ispunjava uslov!\n";
-		std::cout << "Procenat: " << 100. * bouquet4.getEarnings() / bouquet4.getBuyPrice() << "%\n";
+		std::cout << "Procenat: " << bouquet4.getPercentEarnings() << "%\n";
 	}
 	if (cvecara.sellBouquet(1)) {
 		std::cout << "Uspesno prodat buket sa indeksom!\n";
@@ -57,7 +57,7 @@ int main()
 	if (!cvecara.sellBouquet(10)) {
 		std::cout << "Neuspelo prodat buket, los indeks!\n";
 	}
-
+	/*Proveri sto su svi buketi 0 dinara*/
 	std::cout << "Cvecara:\n";
 	std::cout << cvecara << std::endl;
 }
