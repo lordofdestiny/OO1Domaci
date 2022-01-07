da009f6f1158b20a8de171270f182c815588de29
lordofdestiny <42172334+lordofdestiny@users.noreply.github.com>
2022-01-07 20:00:53 +0100

    Added getters for user's memebers

--- OO1Domaci3V1/User.h
+++ OO1Domaci3V1/User.h
@@ -13,6 +13,10 @@ namespace ndb {
 		User(User&&)noexcept = default;
 		User& operator=(User const&) = delete;
 		User& operator=(User&&) noexcept = default;
+
+		std::string const& get_name() const { return _name; }
+		std::string const& get_email() const { return _email; }
+
 		friend std::ostream& operator<<(std::ostream& os, User const& user) {
 			return os << '(' << user._name << ')' << user._email;
 		}
