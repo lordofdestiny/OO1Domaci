4b7353f48a614152c30c1761c4caa9c2881c938c
lordofdestiny <42172334+lordofdestiny@users.noreply.github.com>
2022-01-07 15:49:56 +0100

    Added comments

--- OO1Domaci3V2/Vendor.cpp
+++ OO1Domaci3V2/Vendor.cpp
@@ -2,10 +2,13 @@
 
 namespace ndb {
 	void Vendor::process_shipment(Shipment const& shipment) const {
-		auto& shipment_details = get_shipment_details(shipment);
-		auto const& [item, margin, delivery_time] = find_item(shipment.get_item());
-		shipment_details.shipping_time += delivery_time;
-		shipment_details.sprice += item.get_price() * (1+margin);
+		/* Get reference to shipment details */ 
+		auto& shipment_details = get_shipment_details(shipment); 
+		/* Find item in catalogue */ 
+		auto const& [item, margin, delivery_time] = find_item(shipment.get_item()); 
+		/* Update details */
+		shipment_details.shipping_time += delivery_time; // 1 Update time
+		shipment_details.sprice += item.get_price() * (1+margin); // 2 Update price
 	}
 
 	Vendor::CatalogueItem const& Vendor::find_item(Item const& item) const {
