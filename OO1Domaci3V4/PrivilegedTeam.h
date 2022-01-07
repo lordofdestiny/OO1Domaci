c693e733b2d255e06ced8ca471cbe1ed652be96f
lordofdestiny <42172334+lordofdestiny@users.noreply.github.com>
2022-01-07 19:45:49 +0100

    Added a comment

--- OO1Domaci3V4/PrivilegedTeam.h
+++ OO1Domaci3V4/PrivilegedTeam.h
@@ -8,6 +8,7 @@ namespace ndb {
 		PrivilegedTeam(std::string const& name, std::size_t size, int min_value) :
 			Team(name, size), _min_value(min_value) { }
 
+		/* Join a player to the position, only if his value is large enough */
 		virtual PrivilegedTeam& join_player(Player& player, std::size_t position) override;
 	protected:
 		virtual std::ostream& print(std::ostream& os) const override;
