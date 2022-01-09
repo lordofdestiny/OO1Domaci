ccd0487d7edb1d3a5fbff234b3d0079d28b00e91
lordofdestiny <42172334+lordofdestiny@users.noreply.github.com>
2022-01-09 01:08:51 +0100

    Added a getter for message text

--- OO1Domaci3V1/EMailWithText.h
+++ OO1Domaci3V1/EMailWithText.h
@@ -13,6 +13,8 @@ namespace ndb {
 		bool was_sent() const { return _state > EMailState::PREPARING;  }
 
 		void set_text(std::string const& text);
+		/* Check if this getter is allowed/needed */
+		std::string const& get_text() const { return _text; }
 
 		virtual void send_mail() const override;
 
